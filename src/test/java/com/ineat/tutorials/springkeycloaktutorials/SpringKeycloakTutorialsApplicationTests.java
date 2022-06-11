1e275cbc9608b4b6be68d054e086dc6ff2ac563a
Elhadji Bamba BADIANE <elhadjibamba.badiane.prestataire@sfr.com>
2022-06-11 13:59:23 +0200

    first commit

---
+++ src/test/java/com/ineat/tutorials/springkeycloaktutorials/SpringKeycloakTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ineat.tutorials.springkeycloaktutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKeycloakTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
